622a13b42c7a03f8de7c8445cd248793d9b7068a
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-09-07 14:34:44 -0500

    bower install firebase, form now pushing data to Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 'use script';
 
 var fb = new Firebase('https://tc-landingpage.firebaseio.com/');
+
+
+  $('form').submit(function (e) {
+    var $form     = $(e.target),
+        $fullName = $form.find('input[name="fullname"]'),
+        $contactEmail    = $form.find('input[name="email"]'),
+        $contactLocation = $form.find('input[name="location"]'),
+        name = $fullName.val(),
+        email = $contactEmail.val(),
+        location = $contactLocation.val();
+
+        $fullName.val('');
+        $contactEmail.val('');
+        $contactLocation.val('');
+
+        fb.push({name: name, email: email, location: location});
+        e.preventDefault();
+  });
